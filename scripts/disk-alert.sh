a95fac8ae0ab6982e0b225c41337c70e405c5063
Dwaby404 <160078468+Dwaby404@users.noreply.github.com>
2026-03-14 00:22:05 +0200

    Automated the script to scan my disk every hour and lig the info in my disk-alert.log

--- scripts/disk-alert.sh
+++ scripts/disk-alert.sh
@@ -3,7 +3,7 @@
 usage=$(df / | awk 'NR==2 {print $5}' | sed 's/%//')
 
 if [ $usage -gt 80 ]; then
-	echo "The storage threshold 80% have been exceeded. Thu current usage is $usage%"
+	echo "The storage threshold of 80% have been exceeded. Thu current usage is $usage%"
 
 elif [ $usage -eq 80 ]; then
 	echo "The storage threshold have been reached"
