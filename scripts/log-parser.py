1d1b40f0d53233004b4dc77477fbea5162995a52
Dwaby404 <tlotlisoaustonledwaba@gmail.com>
2026-03-16 08:06:25 +0200

    Improved the log-parser.py script to accept user input and handle FileNotFoundError

--- scripts/log-parser.py
+++ scripts/log-parser.py
@@ -2,17 +2,32 @@
 
 import sys
 
+
+if len(sys.argv) < 3:
+    print("Usage: python3 log-parser.py <logfile> <search term>")
+    sys.exit(1)
+
+
+
 filename = sys.argv[1]
-errors = []
+userKey = sys.argv[2].upper()
 
-with open("fake.log", "r") as f:
-    lines = f.readlines()
+keys = []
 
-for i, line in enumerate(lines, start=1):
-    if "ERROR" in line:
-        errors.append(f"{i}: {line.strip()}")
+try:
 
-for error in errors:
-    print(error)
 
-print(f"\nTotal errors found: {len(errors)}")
+    with open(filename, "r") as f:
+        lines = f.readlines()
+
+    for i, line in enumerate(lines, start=1):
+        if userKey in line:
+            keys.append(f"{i}: {line.strip()}")
+
+    for key in keys:
+        print(key)
+
+    print(f"\nTotal {userKey.lower()}s found: {len(keys)}")
+
+except FileNotFoundError:
+    print(f"{filename} not found")
